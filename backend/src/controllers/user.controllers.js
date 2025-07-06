3c9ba0536029870533b06b6ac98aaf33e8b337f6
Ankush <ankush.work@gmail.com>
2025-07-06 11:20:44 +0530

    updated user controller

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -184,7 +184,8 @@ const logout = asyncHandler(async (req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite: "None"
     }
 
     return res
